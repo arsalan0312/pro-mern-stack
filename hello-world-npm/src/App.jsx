3957f523bb64f32799f4c87f00b09353dc1f086f
Khalil Mohammad <khalilmohammadmirza@gmail.com>
2017-06-27 08:49:12 +0500

    React Classes

--- hello-world-npm/src/App.jsx
+++ hello-world-npm/src/App.jsx
@@ -1,6 +1,9 @@
 const contentNode = document.getElementById('contents');
-const continents = ['Africa','America','Asia','Australia','Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
- 
-const component = <p>{message}</p>; // A simple JSX component
-ReactDOM.render(component, contentNode) // Render the component inside the content Node
+class IssueList extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue list.</div>
+        );
+    }
+}
+ReactDOM.render(<IssueList />, contentNode);  //  Render the component inside the content Node
