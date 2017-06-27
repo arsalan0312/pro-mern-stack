3125645e171b3b85065448c55f51b999503ac901
Khalil Mohammad <khalilmohammadmirza@gmail.com>
2017-06-27 06:43:55 +0500

    Support for es2016

--- hello-world-npm/src/App.jsx
+++ hello-world-npm/src/App.jsx
@@ -1,3 +1,6 @@
-var contentNode = document.getElementById('contents');
-var component = <h1>Hello World!</h1>; // A simple JSX component
-ReactDOM.render(component, contentNode); // Render the component inside the content Node
+const contentNode = document.getElementById('contents');
+const continents = ['Africa','America','Asia','Australia','Europe'];
+const message = continents.map(c => `Hello ${c}!`).join(' ');
+ 
+const component = <p>{message}</p>; // A simple JSX component
+ReactDOM.render(component, contentNode) // Render the component inside the content Node
